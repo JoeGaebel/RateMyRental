d73a04afa6e5349c678d621aa818325f27b300f6
joe <joe.gaebel@gmail.com>
2022-07-15 21:14:40 +1000

    WIP: Working on web reqs

---
+++ ui/src/components/Home.spec.ts
@@ -0,0 +1,39 @@
+import type { SpyInstance } from 'vitest'
+
+import { flushPromises, shallowMount } from '@vue/test-utils'
+import Home from './Home.vue'
+import axios from 'axios'
+import type { Property } from '@/components/Home.vue'
+
+describe('HelloWorld', () => {
+  let getSpy: SpyInstance
+  let propertiesMock: Property[]
+
+  beforeEach(() => {
+    // @ts-ignore
+    import.meta.env.VITE_API_URL = 'api-url'
+
+    propertiesMock = [
+      {address: '123 Clam St', comment: 'Great place!'},
+      {address: '20 Rae Street', comment: 'Ok place!'}
+    ]
+
+    getSpy = vi.spyOn(axios, 'get').mockResolvedValue({ properties: propertiesMock })
+  })
+
+  it('searches for properties and renders the results', async () => {
+    const wrapper = shallowMount(Home)
+
+    const searchInput = wrapper.get('[aria-label="property search"]')
+    await searchInput.setValue('20 Rae Street')
+    await wrapper.find('button').trigger('click')
+
+    expect(getSpy).toHaveBeenCalledWith('api-url/api/properties', {params: {q: '20 Rae Street'}})
+
+    await flushPromises()
+
+    const properties = wrapper.findAll('[aria-label="property listing"]')
+    expect(properties[0].text()).toEqual('123 Clam St')
+    expect(properties[1].text()).toEqual('20 Rae Street')
+  })
+})
